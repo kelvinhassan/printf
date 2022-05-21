d5d20ae1e4e4747dac9b351d22e9c8dea28f4a51
julieessie <julienyambura50@gmail.com>
2022-05-21 07:57:53 -0700

    prints the rot13ed string

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1 or otherwise -1
+ */
+int _putchar(char *c)
+{
+return (write(1, c, 1));
+}  
